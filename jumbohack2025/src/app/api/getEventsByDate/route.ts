a9fb5d8a98313b698eba52e1ea1282eabc90a10c
hkittelberger <holdenkittelberger@gmail.com>
2025-11-19 01:01:50 -0500

    all events now display not just one email

--- jumbohack2025/src/app/api/getEventsByDate/route.ts
+++ jumbohack2025/src/app/api/getEventsByDate/route.ts
@@ -3,9 +3,13 @@ import { query } from '../../../lib/query';
 
 export async function GET() {
   try {
+    // const result = await query(
+    //   'SELECT name, date, start_time, description, id FROM event WHERE date >= CURRENT_DATE AND creator = $1 ORDER BY date ASC',
+    //   ['angie.zhang@tufts.edu']
+    // );
+
     const result = await query(
-      'SELECT name, date, start_time, description, id FROM event WHERE date >= CURRENT_DATE AND creator = $1 ORDER BY date ASC',
-      ['angie.zhang@tufts.edu']
+      'SELECT name, date, start_time, description, id FROM event ORDER BY date ASC',
     );
 
     return NextResponse.json(result.rows);
